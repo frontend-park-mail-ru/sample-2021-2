2ac29c58227666696d30338b9e203f3f496f761e
Sergey Volodin <s.volodin@corp.mail.ru>
2021-09-10 20:00:04 +0300

    add lesson 2

--- public/main.js
+++ public/main.js
@@ -1,21 +1,216 @@
-'use strict';
+console.log('lol kek');
 
-const makeBlue = () => {
-    const header = document.querySelector('.header');
-    header.style.color = '#0000cc';
+const root = document.getElementById('root');
+
+const configApp = {
+    menu: {
+        href: '/menu',
+        name: 'Меню',
+        open: menuPage,
+    },
+    signup: {
+        href: '/signup',
+        name: 'Регистрация',
+        open: signupPage,
+    },
+    login: {
+        href: '/login',
+        name: 'Авторизация',
+        open: loginPage,
+    },
+    profile: {
+        href: '/profile',
+        name: 'Профиль',
+        open: profilePage,
+    },
+    about: {
+        href: '/about',
+        name: 'Контакты',
+    },
 }
 
-const makeBigger = () => {
-    const header = document.querySelector('.header');
-    header.style.fontSize = '50px';
+function ajax(method, url, body = null, callback) {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url, true);
+    xhr.withCredentials = true;
+
+    xhr.addEventListener('readystatechange', function() {
+        if (xhr.readyState !== XMLHttpRequest.DONE) return;
+
+        callback(xhr.status, xhr.responseText);
+    });
+
+    if (body) {
+        xhr.setRequestHeader('Content-type', 'application/json; charset=utf8');
+        xhr.send(JSON.stringify(body));
+        return;
+    }
+
+    xhr.send();
 }
 
-const button = document.querySelector('button');
-button.addEventListener('click', (event) => {
-    makeBlue();
-    event.stopImmediatePropagation();
-}, false);
+function createInput(type, text, name) {
+    const input = document.createElement('input');
+    input.type = type;
+    input.name = name;
+    input.placeholder = text;
 
-button.addEventListener('click', (event) => {
-    makeBigger();
-}, false);
+    return input;
+}
+
+
+function menuPage() {
+    root.innerHTML = '';
+
+    Object
+        .entries(configApp)
+        .map(([key, {href, name}]) => {
+            const menuElement = document.createElement('a');
+            menuElement.href = href;
+            menuElement.textContent = name;
+            menuElement.dataset.section = key;
+
+            return menuElement;
+        })
+        .forEach((el) => {
+            root.appendChild(el);
+        })
+    ;
+}
+
+function signupPage() {
+    root.innerHTML = '';
+
+    const form = document.createElement('form');
+
+    const emailInput = createInput('email', 'Емайл', 'email');
+    const passwordInput = createInput('password', 'Пароль', 'password');
+    const ageInput = createInput('number', 'Возраст', 'age');
+
+    const submitBtn = document.createElement('input');
+    submitBtn.type = 'submit';
+    submitBtn.value = 'Зарегистрироваться!';
+
+    const back = document.createElement('a');
+    back.href = '/menu';
+    back.textContent = 'Назад';
+    back.dataset.section = 'menu';
+
+    form.appendChild(emailInput);
+    form.appendChild(passwordInput);
+    form.appendChild(ageInput);
+    form.appendChild(submitBtn);
+    form.appendChild(back);
+
+    root.appendChild(form);
+}
+
+
+function loginPage() {
+    root.innerHTML = '';
+
+    const form = document.createElement('form');
+
+    const emailInput = createInput('email', 'Емайл', 'email');
+    const passwordInput = createInput('password', 'Пароль', 'password');
+
+    const submitBtn = document.createElement('input');
+    submitBtn.type = 'submit';
+    submitBtn.value = 'Зарегистрироваться!';
+
+    const back = document.createElement('a');
+    back.href = '/menu';
+    back.textContent = 'Назад';
+    back.dataset.section = 'menu';
+
+
+    form.appendChild(emailInput);
+    form.appendChild(passwordInput);
+    form.appendChild(submitBtn);
+    form.appendChild(back);
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const email = emailInput.value.trim();
+        const password = passwordInput.value.trim();
+
+        ajax(
+          'POST',
+          '/login',
+            {email, password},
+            (status) => {
+              if (status === 200) {
+                  profilePage();
+                  return;
+              }
+
+              alert('НЕ получилось не фартануло');
+            }
+        );
+
+    })
+
+    root.appendChild(form);
+}
+
+function profilePage() {
+    root.innerHTML = '';
+
+    ajax(
+        'GET',
+        '/me',
+        null,
+        (status, responseText) => {
+            let isAuthorized = false;
+
+            if (status === 200) {
+                isAuthorized = true;
+            }
+
+            if (isAuthorized) {
+                const {age, score, images} = JSON.parse(responseText);
+
+                const span = document.createElement('span');
+                span.textContent = `Мне ${age} и я крутой на ${score} очков`;
+
+                root.appendChild(span);
+
+                const back = document.createElement('a');
+                back.href = '/menu';
+                back.textContent = 'Назад';
+                back.dataset.section = 'menu';
+
+                root.appendChild(back);
+
+                if (images && Array.isArray(images)) {
+                    const div = document.createElement('div');
+                    root.appendChild(div);
+
+                    images.forEach((imageSrc) => {
+                        div.innerHTML += `<img src="${imageSrc}"/>`;
+                    })
+                }
+
+                return;
+            }
+
+            alert('АХТУНГ НЕТ АВТОРИЗАЦИИ');
+            loginPage();
+        }
+    );
+}
+
+menuPage();
+
+
+
+root.addEventListener('click', e => {
+    const {target} = e;
+
+    if (target instanceof HTMLAnchorElement) {
+        e.preventDefault();
+
+        configApp[target.dataset.section].open();
+    }
+})
--- server/index.js
+++ server/index.js
@@ -1,25 +1,110 @@
-const http = require('http');
-const fs = require('fs');
-const debug = require('debug');
+'use strict';
 
-const log = debug('*');
+const express = require('express');
+const body = require('body-parser');
+const cookie = require('cookie-parser');
+const morgan = require('morgan');
+const uuid = require('uuid/v4');
+const path = require('path');
+const app = express();
 
-const page404 = fs.readFileSync('./public/404.html');
+app.use(morgan('dev'));
+app.use(express.static(path.resolve(__dirname, '..', 'public')));
+app.use(body.json());
+app.use(cookie());
 
-const server = http.createServer((req, res) => {
-    const path = req.url === '/' ? '/index.html' : req.url;
-    log('request', req.url, path, req.headers['user-agent']);
+const users = {
+    'd.dorofeev@corp.mail.ru': {
+        email: 'd.dorofeev@corp.mail.ru',
+        password: 'password',
+        age: 21,
+        score: 3,
+    },
+    's.volodin@corp.mail.ru': {
+        email: 's.volodin@corp.mail.ru',
+        password: 'password',
+        age: 24,
+        score: 100500,
+        images: [
+            '/server/images/241239898_509534673414004_2670816557845118956_n.jpeg'
+        ]
+    },
+    'aleksandr.tsvetkov@corp.mail.ru': {
+        email: 'aleksandr.tsvetkov@corp.mail.ru',
+        password: 'password',
+        age: 27,
+        score: 72,
+        images: [
+            'https://scontent-arn2-1.cdninstagram.com/v/t51.2885-15/e35/19228555_310775326029169_611501797038620672_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=101&_nc_ohc=CSWYhhZaI30AX-8TUvX&edm=AP_V10EBAAAA&ccb=7-4&oh=eec5a05513bbcbdf4e4ed5810fb67c4a&oe=613FF7EB&_nc_sid=4f375e',
+            'https://scontent-arn2-1.cdninstagram.com/v/t51.2885-15/e35/16583858_168051673696142_846500378588479488_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=102&_nc_ohc=h_mjiDqypmwAX_Lz_ea&edm=AP_V10EBAAAA&ccb=7-4&oh=e77934079b4d97b532b3d205ac6e79e0&oe=6140875B&_nc_sid=4f375e',
+            'https://scontent-arn2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s750x750/13259619_1728960653986462_1525963650_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=110&_nc_ohc=ZYeN4ZWTY6oAX9UqUjI&tn=pmbb9kppgOfU9eNB&edm=AP_V10EBAAAA&ccb=7-4&oh=0acdeffc5d5f4e3bcdb984f1c6727be1&oe=61405C3A&_nc_sid=4f375e',
+        ],
+    },
+    'a.ostapenko@corp.mail.ru': {
+        email: 'a.ostapenko@corp.mail.ru',
+        password: 'password',
+        age: 21,
+        score: 72,
+    },
+};
+const ids = {};
 
-    fs.readFile(`./public${path}`, (err, data) => {
-        log('file was read');
-        if (err) {
-            data = page404;
-        }
+app.post('/signup', function (req, res) {
+    const password = req.body.password;
+    const email = req.body.email;
+    const age = req.body.age;
+    if (
+        !password || !email || !age ||
+        !password.match(/^\S{4,}$/) ||
+        !email.match(/@/) ||
+        !(typeof age === 'number' && age > 10 && age < 100)
+    ) {
+        return res.status(400).json({error: 'Не валидные данные пользователя'});
+    }
+    if (users[email]) {
+        return res.status(400).json({error: 'Пользователь уже существует'});
+    }
 
-        res.write(data);
-        res.end();
-    });
+    const id = uuid();
+    const user = {password, email, age, score: 0, images: []};
+    ids[id] = email;
+    users[email] = user;
+
+    res.cookie('podvorot', id, {expires: new Date(Date.now() + 1000 * 60 * 10)});
+    res.status(201).json({id});
 });
 
-log('listening at http://127.0.0.1:8080')
-server.listen(8080);
+app.post('/login', function (req, res) {
+    const password = req.body.password;
+    const email = req.body.email;
+    if (!password || !email) {
+        return res.status(400).json({error: 'Не указан E-Mail или пароль'});
+    }
+    if (!users[email] || users[email].password !== password) {
+        return res.status(400).json({error: 'Не верный E-Mail и/или пароль'});
+    }
+
+    const id = uuid();
+    ids[id] = email;
+
+    res.cookie('podvorot', id, {expires: new Date(Date.now() + 1000 * 60 * 10)});
+    res.status(200).json({id});
+});
+
+app.get('/me', function (req, res) {
+    const id = req.cookies['podvorot'];
+    const email = ids[id];
+    if (!email || !users[email]) {
+        return res.status(401).end();
+    }
+
+    users[email].score += 1;
+
+    res.json(users[email]);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+    console.log(`Server listening port ${port}`);
+});
